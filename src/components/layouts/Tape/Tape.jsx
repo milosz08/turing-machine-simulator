9b69a291bd3cc5a925fb9473b36de18d845f9a31
Milosz08 <gilgamilosz451@gmail.com>
2021-08-29 20:11:58 +0200

    initialised Tape component (with styled-components styles)

---
+++ src/components/layouts/Tape/Tape.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { TapeContainer } from './Tape.styles';
+
+const Tape = () => {
+    return (
+        <TapeContainer>
+            Tape Container
+        </TapeContainer>
+    );
+};
+
+export default Tape;
