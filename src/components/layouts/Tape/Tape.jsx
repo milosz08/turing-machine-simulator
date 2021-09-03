0433d30a1e861b54947e9ebf373e9490a65471da
Milosz08 <gilgamilosz451@gmail.com>
2021-09-03 05:11:11 +0200

    add comments, cleaning and finishing

--- src/components/layouts/Tape/Tape.jsx
+++ src/components/layouts/Tape/Tape.jsx
@@ -1,8 +1,25 @@
+/**
+ * @file Tape.js
+ * @author Miłosz Gilga (gilgamilosz451@gmail.com)
+ * @brief JavaScript React Stateless functional component (simplify state with React Hooks).
+ *
+ * @projectName "turing-machine-simulator-react-js"
+ * @version "^0.1.0"
+ *
+ * @date 09/03/2021
+ */
+
 import React, { useContext } from 'react';
 import { StoreContext } from '../../store/StoreProvider';
 
 import { TapeContainer, TapeCharacter } from './Tape.styles';
 
+/**
+ * @details Component responsible for generating the appearance of the tape and positioning the head over the 
+ *          supported element, according to the value in the React Context store.
+ * 
+ * @returns { JSX.Element }
+ */
 const Tape = () => {
 
     const { tapeValue, headPosition } = useContext(StoreContext);
--- src/components/layouts/TapeInfos/TapeInfos.jsx
+++ src/components/layouts/TapeInfos/TapeInfos.jsx
@@ -1,8 +1,25 @@
+/**
+ * @file TapeInfos.js
+ * @author Miłosz Gilga (gilgamilosz451@gmail.com)
+ * @brief JavaScript React Stateless functional component (simplify state with React Hooks).
+ *
+ * @projectName "turing-machine-simulator-react-js"
+ * @version "^0.1.0"
+ *
+ * @date 09/03/2021
+ */
+
 import React, { useContext } from 'react';
 import { StoreContext } from '../../store/StoreProvider';
 
-import { TapeInfosContainer, StateInfosWrapper } from './TapeInfos.styles';
+import { TapeInfosContainer, StateInfosWrapper, MessageWrapper } from './TapeInfos.styles';
 
+/**
+ * @details Component responsible for generating a field with information about the previous and next label, 
+ *          information about the number of states and the main text information about the state of the machine.
+ * 
+ * @returns { JSX.Element }
+ */
 const TapeInfos = () => {
 
     const { machineEndMessage, actualState, allSteps } = useContext(StoreContext);
@@ -16,7 +33,7 @@ const TapeInfos = () => {
             <StateInfosWrapper pseudoContent = 'labels info'>
                 {labelsInfo}
             </StateInfosWrapper>
-            <div>{machineEndMessage}</div>
+            <MessageWrapper>{machineEndMessage}</MessageWrapper>
             <StateInfosWrapper pseudoContent = 'states count'>
                 {allSteps.length}
             </StateInfosWrapper>
