5eda29fe06893793d019e7254bf74c588b1b8e5e
Milosz08 <gilgamilosz451@gmail.com>
2024-02-15 06:55:26 +0100

    remove __ prefix from build output files

--- .webpack/webpack-common.config.cjs
+++ .webpack/webpack-common.config.cjs
@@ -15,10 +15,8 @@ module.exports = isProd => ({
   },
   output: {
     path: path.join(__dirname, '..', 'dist'),
-    filename: `js/__react-[${isProd ? 'contenthash:10' : 'name'}]-bundle.js`,
-    chunkFilename: `js/__react-[${
-      isProd ? 'contenthash:10' : 'name'
-    }]-chunk.js`,
+    filename: `js/react-[${isProd ? 'contenthash:10' : 'name'}]-bundle.js`,
+    chunkFilename: `js/react-[${isProd ? 'contenthash:10' : 'name'}]-chunk.js`,
     publicPath: '/',
     clean: true,
   },
